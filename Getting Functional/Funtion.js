d5beeb0777258357e334adf403520cfbacf4dc9d
Yamanoban-17 <yanamase96@gmail.com>
2025-03-24 15:30:25 +0800

    Function Activity 1

---
+++ Getting Functional/Funtion.js
@@ -0,0 +1,47 @@
+/*
+
+
+var dogName = " hiro";
+var dogweight = 21;
+if (dogweight > 20) {
+    console,log(dogName + " says WOOF WOOF");
+} else {
+    console.log( dogName + " says WOOF WOOF")
+}
+
+var dogName = " hiro";
+var dogweight = 21;
+if (dogweight > 19) {
+    console,log(dogName + " says WOOF WOOF");
+} else {
+    console.log( dogName + " says WOOF WOOF")
+}
+
+var dogName = " hiro";
+var dogweight = 21;
+if (dogweight > 25) {
+    console,log(dogName + " says WOOF WOOF");
+} else {
+    console.log( dogName + " says WOOF WOOF")
+}
+
+var dogName = " hiro";
+var dogweight = 21;
+if (dogweight > 26) {
+    console,log(dogName + " says WOOF WOOF");
+} else {
+    console.log( dogName + " says WOOF WOOF")
+}
+*/
+    function bark(name,weight) {
+        if (weight > 20) {
+            console,log(name + " says WOOF WOOF");
+        } else {
+            console,log(name, + " says WOOF WOOF");
+        }
+    }
+
+bark(" hiro,23")
+bark(" lorenz",2)
+bark(" rym",18)
+bark(" vicente",15)
