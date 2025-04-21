c5e593bc78e5b7dc123080dafd24ab12046c5859
Yamanoban-17 <yanamase96@gmail.com>
2025-04-21 10:44:24 +0800

    battleship_v3

--- battleship/battleshipv2.js
+++ battleship/battleshipv2.js
@@ -1,36 +1,52 @@
-// declaring variable
-var location1 = 3;
-var location1 = 4;
-var location1 = 5;
+var randomLoc = Math.floor(Math.random() *5);
 
-var guess 
-var hit = 0;
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
+
+var guess;
+var hits = 0;
 var guesses = 0;
 
 var isSunk = false;
 
 // Game logic
 
-while(isSunk == false){
-   guess = prompt("Ready!. Aimi. Fire! (enter number 0-6)");
-   if (guess < 0 || guess > 6){
-       alert ("lease enter a valid call number");
-   }else {
-     guesses = guesses + 1;
-     alert("hits")
-     if(guess == location || guess == location || guess == location) {
-        hits = hits + 1;
-        if(hits == 3){
-            inSunk = true;
-            alert ("You sunk my battleship")
+while (isSunk == false){
+    guess = prompt("Ready!, Aim, Fire! (Enter a number 0-6): ");
 
+    if (guess == null) {
+        alert("Thank you for playing!");
+        break;
+    }
+
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!");
+    }else {
+        guesses = guesses + 1;
+        if(guess == location1 || guess == location2 || guess == location3){
+            hits = hits + 1;
+            alert ("hits")
+            if (hits == 3) {
+                inSunk = true;
+                alert("You sank my battleship!")
+            }
         }
-     }
-   }
+        else{
+          
+            alert("Miss!")
+            if (guesses >= 10) {
+                alert("You exceeded the maximum attempts!");
+                break;
+            }
+        }
+    }
 }
 
-var stats = "You sank my battleship at: " + guesses + "guesses" + "and your accuracy is:" + (3/guesses);
+var accuracy = (guess > 0 ? (hits / guess) * 100 : 0). toFixed(3);
+
+var stats = "You took " +guesses + " guesses to sink the battleship! " + "\n" + "Accuracy: " + accuracy + "%";
+
+var stats = "You sank my battleship at: " + guesses + "guesses" + "and your accuracy is: " + (3/guesses);
 
 alert(stats);
-
-alert("hit the location")
